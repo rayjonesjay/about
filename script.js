8878b1c40f583e4bdcfe47a01bc1589c57c91640
rayjonesjay <rayjaymuiruri@gmail.com>
2024-11-13 06:28:28 +0300

    add: files

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const name = "RAY JONES MUIRURI";
+    const writerElement = document.getElementById("name-writer");
+
+    function autoTypeEffect(text, delay=3000) {
+        let i = 0;
+        writerElement.textContent = "";
+        const type = () => {
+            if (i < text.length) {
+                writerElement.textContent += text[i];
+                i++;
+                setTimeout(type,100); //type speed
+            }else {
+                setTimeout(() => {
+                    writerElement.textContent = "";
+                    i = 0;
+                    type();
+                }, delay);
+            }
+        };
+        type();
+    }
+
+    autoTypeEffect(name);
+})
